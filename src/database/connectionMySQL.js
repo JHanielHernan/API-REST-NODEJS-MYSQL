f17064b542a5d4d04a6dfb45e0704c04ece53934
JHanielHernan <cuentan1f0600@gmail.com>
2024-12-11 17:55:53 -0600

    Agregar env

--- src/database/connectionMySQL.js
+++ src/database/connectionMySQL.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import {createPool} from 'mysql2/promise';
 
 export const pool = createPool({
-    host:"localhost",
-    port:3306,
-    database:"tareas",
-    user:"root",
-    password:""
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    database:process.env.DB_NAME,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS
 })
